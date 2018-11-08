3738065a5475cd58041ed32794b14d960952472e
Kirifraise <44511526+Kirifraise@users.noreply.github.com>
2018-11-08 19:12:20 +0100

    Add files via upload

---
+++ enfin/menu.php
@@ -0,0 +1,12 @@
+<?php
+$surnom= $_POST["rep"];
+
+if($surnom == "amour"){
+  echo "Bon courage mon " . $surnom . " de ma vie";
+}elseif ($surnom == "coeur") {
+  echo "Bonne journée petit " . $surnom;
+}
+elseif($surnom =="nounouille"){
+	echo "Que je t'aime ma ".$surnom." d'amour";
+}
+?>
